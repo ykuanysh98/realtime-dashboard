6affb2ed973d5e0a975253a8b08eebd3a0c1dee3
Kuanyshbek <it13@kulanoil.kz>
2026-06-09 10:37:01 +0500

    feat: add app layout with conditional sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
+<template>
+  <div class="app-layout">
+    <AppSidebar v-if="auth.isAuthenticated" />
+    <main class="app-main">
+      <router-view />
+    </main>
+  </div>
+</template>
+
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import AppSidebar from './components/AppSidebar.vue'
+import { useAuthStore } from './stores/auth.store'
+const auth = useAuthStore()
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<style>
+.app-layout {
+  display: flex;
+  min-height: 100vh;
+}
+.app-main {
+  flex: 1;
+  overflow: auto;
+  padding: 32px;
+}
+</style>
